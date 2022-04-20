e7e94e035801b661c257bb64e11a96c808a37dd1
Devin Stein <devstein@seas.upenn.edu>
2022-04-20 11:22:58 -0700

    Remove code supporting IE 11

--- src/crypto.ts
+++ src/crypto.ts
@@ -1,13 +1,7 @@
 // Adapted from https://github.com/auth0/auth0-spa-js/blob/master/src/utils.ts#L143
-export const getCrypto = () => {
-  //ie 11.x uses msCrypto
-  return (window.crypto || (window as any).msCrypto) as Crypto;
-};
-
 export const getCryptoSubtle = () => {
-  const crypto = getCrypto();
   //safari 10.x uses webkitSubtle
-  return crypto.subtle || (crypto as any).webkitSubtle;
+  return window.crypto.subtle || (window.crypto as any).webkitSubtle;
 };
 
 export const randomString = () => {
@@ -15,7 +9,7 @@ export const randomString = () => {
     "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_~.";
   let random = "";
   const randomValues = Array.from(
-    getCrypto().getRandomValues(new Uint8Array(43))
+    window.crypto.getRandomValues(new Uint8Array(43))
   );
   randomValues.forEach((v) => (random += charset[v % charset.length]));
   return random;
