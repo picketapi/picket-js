995716b204f0dc4c7c427694e523b6d1b335ea97
Devin Stein <devstein@seas.upenn.edu>
2022-09-15 11:12:56 -0700

    Refactor Powered by component

---
+++ src/connect/PoweredByPicket.tsx
@@ -0,0 +1,22 @@
+import { tw } from "twind";
+
+const PoweredByPicket = () => {
+  return (
+    <div
+      className={tw`w-full mt-8 text-center font-base text-sm text-gray-400`}
+    >
+      <a
+        style={{
+          outlineOffset: "4px",
+        }}
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://picketapi.com"
+      >
+        Powered by Picket
+      </a>
+    </div>
+  );
+};
+
+export default PoweredByPicket;
