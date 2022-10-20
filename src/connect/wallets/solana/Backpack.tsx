7caebd7d997e845c8809df9f3d53a21a0c740559
Devin Stein <devstein@seas.upenn.edu>
2022-10-20 15:24:30 -0700

    Add Glow and Backpack solana wallets

---
+++ src/connect/wallets/solana/Backpack.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BackpackWalletAdapter } from "@solana/wallet-adapter-wallets";
+
+import { SolanaWalletAdpaterWallet } from "../../wallets";
+
+const color = "#e23f3f";
+
+const wallet = new SolanaWalletAdpaterWallet({
+  // Ignoring the type difference for now
+  // BackpackWalletAdapter sendTransaction is extends BaseMessageSignerWalletAdapter in an incompatible way
+  adapter: new BackpackWalletAdapter(),
+  color,
+});
+
+export default wallet;
